d338144a35f385c1a6d18f915997acefc4c33d0b
DretcmU <alexander.carpio@ucsp.edu.pe>
2024-12-19 21:58:46 -0500

    last class

--- app/main.py
+++ app/main.py
@@ -15,3 +15,5 @@ def run():
 
 if __name__ == '__main__':
     run()
+    print("already 123")
+    print()
--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una pagina 123456</h1>
+        <p>soy un parrafo</p>
+    """
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+    
 def run():
     store.get_categories()
 
